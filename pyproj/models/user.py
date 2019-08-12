23c9dd9a5c5f983f4a4a512a7fa34098571f52e7
Senan K <senank@ronic.mail>
2019-08-12 10:30:49 +0300

    user table generated

--- pyproj/models/user.py
+++ pyproj/models/user.py
@@ -11,8 +11,7 @@ from .meta import Base
 
 class User(Base):
     __tablename__ = 'user'
-    # __table_args_ = {extend_existing : True}
     user_id = Column(Integer, primary_key=True)
-    first_name = Column(Text)
-    last_name = Column(Text)
+    username = Column(Text)
+    password = Column(Text)
     permissions = Column(Text)
